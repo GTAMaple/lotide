f692f9415a03bcb7b6ef189c8ad45de9acfec082
GTAMaple <sasi.pjn@gmail.com>
2021-10-26 13:00:39 +0000

    Template Literals (Template Strings)

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,12 @@
 const assertEqual = function(actual, expected) {
   
   if (actual === expected) {
-    console.log("✅Assertion Passed:" + actual + "=== " + expected);
+    //console.log("✅Assertion Passed:" + actual + "=== " + expected);
+    console.log(`✅Assertion Passed:, ${actual} ===  ${expected}`);
+    //`Hello, ${name}!`
   } else {
-    console.log("❌Assertion Failed:" + actual + "!== " + expected);
+    //console.log("❌Assertion Failed:" + actual + "!== " + expected);
+    console.log(`❌Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
 
