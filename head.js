743085fbba9ce47dd70ab7851ee719c685d63bf2
GTAMaple <sasi.pjn@gmail.com>
2021-10-26 14:05:09 +0000

    Implement head using function inside another function

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+
+    console.log(`✅Assertion Passed:, ${actual} ===  ${expected}`);
+
+  } else {
+
+    console.log(`❌Assertion Failed:, ${actual} !== ${expected}`);
+  }
+};
+
+//head function will be executed first and then assertEqual function will be executed later
+const head = function (array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
