b93abaccf569c1fc530e8e2155bf07e87a173f6c
GTAMaple <sasi.pjn@gmail.com>
2021-10-28 13:12:12 +0000

    check if to find the value of key = true

--- countOnly.js
+++ countOnly.js
@@ -14,11 +14,14 @@ const countOnly = function (allItems, itemsToCount) {
   let resultObj = {};
 
   for (let key in itemsToCount) { // iterate to "key" inside Obj ex. Jason, Karima, Fang
-
-   
+    //console.log(key);
+    if (itemsToCount[key]) { //if the "value of key" is = true, then look for "key" is inside array
+    console.log(key);
+    console.log(itemsToCount[key]); 
+    }
 
   }
-  
+
 }
 
 const firstNames = [
@@ -35,7 +38,7 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
